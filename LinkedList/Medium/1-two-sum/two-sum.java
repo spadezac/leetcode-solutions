8a47016194a4486a93310adba0b5549493df1bfa
Simran Gupta <113887915+spadezac@users.noreply.github.com>
2025-07-16 17:19:35 +0530

    Time: 2 ms (98.96%) | Memory: 45.1 MB (47.09%) - LeetSync

--- LinkedList/Medium/1-two-sum/two-sum.java
+++ LinkedList/Medium/1-two-sum/two-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
             int complement = target - nums[i];
-            for(int j = 0; j < nums.length; j++){
-                if(nums[j] == complement && i != j){
-                    return new int[]{i, j};
-                }
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
         return new int[]{};
     }
